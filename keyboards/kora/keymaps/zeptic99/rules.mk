16b865a645a9f891b786c838428fc1d02f2a658e
zeptic99 <taseenrashed@gmail.com>
2024-08-24 06:04:06 +0600

    Zepmash rev 11

---
+++ keyboards/kora/keymaps/zeptic99/rules.mk
@@ -0,0 +1,8 @@
+MOUSEKEY_ENABLE = no    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = ssd1306
+LTO_ENABLE      = yes
+
+ENCODER_ENABLE = yes
+OLED_TRANSPORT = i2c
